51c30f06dd60754618ce144f93700c2303e5be5a
Morre Thierry <thierry.morre@cognitic.be>
2022-08-29 09:52:20 +0200

    First Commit

---
+++ ScriptDB.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE DemoContact;
+GO
+
+Use DemoContact;
+Go
+
+CREATE TABLE [User]
+(
+    Id INT NOT NULL IDENTITY,
+    Email NVARCHAR(384) NOT NULL,
+    Passwd BINARY(64) NOT NULL,
+    CONSTRAINT PK_User PRIMARY KEY (Id),
+    CONSTRAINT UK_User_Email UNIQUE (Email)
+);
+
+CREATE TABLE [Contact]
+(
+    ID INT NOT NULL IDENTITY,
+    LastName NVARCHAR(50) NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
+    BirthDay DATE NOT NULL,
+    Email NVARCHAR(384) NOT NULL,
+    Phone NVARCHAR(20) NULL,
+    UserId INT NOT NULL
+    CONSTRAINT PK_Contact PRIMARY KEY (Id),
+    CONSTRAINT FK_Contact_User FOREIGN KEY (UserId) REFERENCES [User](Id)
+);
+
+GO
+
+CREATE FUNCTION TSF_GetPreSalt()
+RETURNS NCHAR(2048)
+AS
+BEGIN
+    RETURN N'hS#|5z@JbFK2+NOw1@lzOKefSlXOifqQGoPM6UyrWodCi*V4v5Lu%FOTMPfiRLIJdCZ3R|ziB?75E8dUUjCGRu2Kl#u!j|zcitOdxNyF?xZ@AciHocnzTB6UEwVLbl|uQub-yvWNqtzEbmse+pdDylp-ihVjhMjqhech4Ul#Xk!lYareow*?!M#I*86!3UJ%yVkAuy+bE$a6%Zq4d5gukIOl&GNJ0SHHsEnB*1EXN7GkycETDSye9RxxNfoAaxwBD81?AIx|W$MPDC+*+uM#GrC7noVyNx%DX5txa@vkJ#o%Chgz+Kaa6q2NtKUh-qdKNfCXEP7tpiH9rJ#m7XAnumjaO$-SOxkAoJsMx9c4F4QrT4L8rTvgvHOEilHs$s3uhGrMInOgCaFQSW1fJAZr1QBWv!HWkgVAQEtW$CWFrmTdxS0vXR1uVeWCyL7#n2K1@3ZsrBvGwwAo4mRkkh-m#Y3K4iuv&w+eEKiyc%00yu+!AhN+q*8Z9r&?-M+Ejyy#ySn1h2@#lz4qR$x1sc#Q%PUBr$o?2M!5O#@#Ws&fNddHQ&ARqHqCAk$H@Zi9twQttyHjzb7fHGxfbZ2mFsH*45Z$DEAyHchCiZDbGM*BI|2*6H*lOK#vS4-l8bttBz!4$imd*??ok+r*4G-3OS37@25kQWX-R635CPo$Wm3ee+e4R-$C@Gb3evK0HJneUNmb2R|5gV-QDsO1*F*&DH-OrD#eWG!ROL4&jF6SY|!buOu&sU1l@#@y1*3GJVWKLLK+N9-3dHa6VBYZEg6P?7RuTbMj1I#%$w7Tyq2@OqIhJdYG3xy$&&|AjsP|U+G|35QuN?LBdOpsEZYFo|vUD#ms$a?CXc3cuQMZnG4PdrD!dS2r1mqfPYns5pbWD0X7B?@q@!sgkw$#mwK7fC75g$bFQxE%5dWdsUSMsPIxEQh4cwTcuJCBtEt90WiTg?$S!p|zEHpOClFWKkX93+yBmUsRAFN%cb4APHiDfxbIfOvB7Yp7VgRrEzo|r+1Q0F8WKgUy17B1Sm*!14B8ZZ&$cP*3Z?S572kYGAhWjC5rjlplcU+H-|EFqIc4MWZCRfOX0kK9L!IgnyFceA65tePZR#crB*1cn|5B-VVOk2|0KNH1Qx-IC4WrCsvhmXE*voJPteE-qM*sEV+S87BE6rMSzJ634qIB8AU1FYlODpT+&Z1oqT&NMcPDAROjPlGgeWEFL9luiw-Z*VC$BD!|7X4@r1HZa539T145pXBfC&e3ZCP3PrG7*@QRgVaOaD5I5L4MOJbzTw+A@#x15i!lU8ySP0wA5mB&w?%wN8Zve@!yPOnwKTlj+NjVv7!kYCXMubbNPt+3x6@MRNJyl-+#Z77+5hs*i*&I@pJ5jS+lJ+V8!PEKYKWR|XFG5SolR3IQgysFJpYv?fa*VYAH5wDxQE3!q%NfAE&0XkkwvSVkAg@u-uEgu7H|6l7BnLV7iQJcaB2IkeJEBQ2b64Jl?a0Ppvlz1k?ThwbHEzRxvGQum#MfAa5nW75Q04lOZdqCNSwdcUgZ2xoRjFXq4e0+INsL+#mSW$6Z&E*lHMu@kMn$rY-8XPe0nPs-2jTKk!opcdJ7VlE-%5cnfFT%|LtEUy@?CN!4A4qsYTDUh4jP4PymtmdU3Iu#bkSZ0T-eZ3@+n5vBPM5tFd+x+w%O@aDjJpVtic|cnxlCOlUQO?!rTDr0pf25f!j#udffRnrTwtOZv2!8!39FTs5?mXi@@guEmuqbzQ0!oHCYPYgJsqH1NB5zzE3QseQcEsR075Wr$3QIDS3KLtFRlVJjCACv|tQcRYc8@Sl@pd03HDJ$4&dM2s8a4!wOFVhU2EQQeb7S6?NdgVgCOa5q-p5NxzBwIAK1!?fzGF#VMNJZn7Ip9Kq?FBwNzLN?jL0vW4Mv-f?&DlRmj*JKHfijfAeSscXb1FM+AM0Jbq?FI5#1!-fHwd-39FR+35HGxVBoxTCKUSzhUSk8a#67BHo&-rNz9Ja5J1CHpbii1cDv!+R*CYYHzPJIja|3uX%zLLslc5U|lowW403H5%ivhmyDr**p';
+END
+GO
+
+CREATE FUNCTION TSF_GetPostSalt()
+RETURNS NCHAR(2048)
+AS
+BEGIN
+    RETURN N'eME8hI5?R-bT?RXis*wjy@lT-g%?q3SGJ7imEs7xe%0m@ZhxSPfod|pyi&U%dfByW!X7c1aBX8W|uKS6vD0Rw$A8v0T!mIN7kfIdBj3o#R9ASh-vQWUtQEZo!pqU#4*CXrDd$-c412@diX?Zv3MG!Tw#B2qQvj&!BDp6o!M|EUAluuYhYR6eR#HSy5-hSwSIoN0vYjPP1gaa8Tb0lw@XQ30@iVu&MmheMhEDNuwYO!5AXnG@Ck#mupG1h6ZAM7Wdh?XnEb-#I$gSfh!Ir9A*$d8#yyr!@%FI0IEhDH2vxvRQ6c7BkuXAjx3b3QJUHuK7l1?6lU@sh0X-Bw|dus&G64BbdzG&&2&PqQrI&TmD?Vv3@KZv3#?cnGy-b%qi-YTiwWB7w1tU1CAUeL7zC3NkD|&r1A$d!B8T7F9pIbU8%xF48wZi+pYbv|F7X3iJ@vI8IM!4u|OOg69SdDcx@ZpaDtT46BU12XbJ?VrTejGD1waXz|&2KMU@@XO+wcW|?s0+GXrJ3Lek&%pGyWViA?XD?DCP7A@$F4y4aGaviSOjY8mcqgS5U2zDRoXgE5TW-0ei*v6WCS*UMwy@x+$AtuP&Vk8oR|r?pIJdXY&Yv&ujmoh|marYfRa-pla!#5$fts-GUxIQQAL|sAUsjD+?6%B2PsKsd5-J5wY72KWaFv$GhMXP06Jyt&CAlxOK@V3Zrjshn!fOsk#n5w-&0Ez+XLDUR50pnfl1W+S&%RSuK#hGpQD3|6|&&1+jijXWB4jnZeNt&0Kpw@J6LU8wekvNr7HeacO-sV|3NpWIl$i9gQTPxD%9xddnA%Y5jMDK|BG+5C4OGWd|$*opI+sE*sPE$JjucAc|JvzqUl0C|P&r0WM4LcVdl$II7pnBkkkuBS2XALeBTGU+C5zk-UUQp7RieGegryXXqqRWNkHC&&8iopaqApdrxoDtG1BIbW$iDcCYG4KmY#DR8SHcU#&7otpW6L%D9bf#XaH5WCsYBDe2TGCTd0E|6$q44X8y9cybhN!U#rfRu2i012WI6qGIo!F#%1vrCrk#|wDR9EV05x&fzJ1dT9eZs5e5Gh$ZQdG-WKIfWyZW%ybLIlZg@eZXJX0lYdccwKQT3%YOW?3i+I$c?r3kXfpvyX7t1&xqIMR58QwUIq?+HExNlxwHxFeC3giFun&iyW!CQcKbhl8l9tpul2zNYG-yfn2?Xr-Hzg4ub+-FP+SR8k0KKDDLDU!l4Bi!hHHK|qWrJiDupmpCX5&xvzSWoR+#u-hJTz+J9kKggd7D1+4BMIVB189zPaR&-ymTq#JwSXQ#E3F!o4SmMI6g@iQ@@z$w+rwfvPWoS3Ut*loLg4dUDINsY#GhaQwF9V25sQIqltdG|h%Ws06ExYvvl?OCjM*pFnkLY!doHY*jb&i6#4cm04J3%ZNsGcP8equKqX2J3Owtulvsjc6Ze9DU3VkP1SLut1QP*Q9tqP@*?P?@Q5XabWm7|6V!w0fHd*PVf55av!Sdq1knJ5+o-kkp39L7pILH3wuQKlPMq6&j-Z!gx221*!D+X2%h4Ze8kbiuaBLowAXXH2D6u%pA%OXWr8v3q&FE%oTXm14ud1SZO@9xOZ1Y3VQiNG1uf8JU&%tY&W6X&#aSaG1QT&7B%68isC#k-eIjpTHmjvreDKt+Tqq5CPv?THKRTet6|eZfPPGnVP9tpA-GKA12RNi1owH#o3f9tiHfsN*f0I$KTU9#2T-SEHjFHkdr+v|Tz4ycx?T7kKfGE%zQZ3o1h7V@?rQvGxs5MdUcIyv*!md1*L7c*USQqTKo33atS4vyRkEc?YRMP98tnfjUP5lemrHyeu6ZgM9cgVeyXFVVQ+9TplYZ##5qx28GzQHA3b4Bg1eSn&K4Jql+0dX0z0bpiHs%Db*abUa+&gX3nWOMtUyXcNF%UkX2?%v0nV+DbbU$79pxUL7ogGsybzTFp2W-6XEid6!V?0|T9D@ZzlCQNf6uRJMqhBw&CD#1ihxMV!ZoS&BIlpV8&7#*eU5Tn|T&y#ZF7kos8tpRcgUu?Nm9lH$xD&@m|t378Ngl';
+END
+GO
+
+CREATE PROCEDURE TSP_Register
+    @Email NVARCHAR(384),
+    @Passwd NVARCHAR(20)
+AS
+BEGIN
+    INSERT INTO [User] (Email, Passwd) VALUES (@Email, HASHBYTES('SHA2_512', dbo.TSF_GetPreSalt() + @Passwd + dbo.TSF_GetPostSalt())); 
+END
+GO
+
+CREATE PROCEDURE TSP_Authorize
+    @Email NVARCHAR(384),
+    @Passwd NVARCHAR(20)
+AS
+BEGIN
+    SELECT Id From [User]
+    WHERE Email = @Email AND Passwd = HASHBYTES('SHA2_512', dbo.TSF_GetPreSalt() + @Passwd + dbo.TSF_GetPostSalt()); 
+END
+GO
